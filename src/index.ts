36a581fe5316e388cb7585af6d30f006660fc6cf
Kopcsak Patrik <kopcsakpatrk1@gmail.com>
2022-10-19 12:28:33 +0200

    first commit

---
+++ src/index.ts
@@ -0,0 +1,61 @@
+
+interface Multimedia
+{
+    cim: string;
+
+    kiir(): void;
+    toString(): string;
+
+
+}
+
+class Zene implements Multimedia
+{
+    cim: string;
+    hossz: number;
+
+    constructor(cim: string, hossz: number)
+    {
+        this.cim = cim;
+        this.hossz = hossz;
+    }
+
+    kiir(): void {
+       console.log(this.toString());
+    }
+    toString(): string {
+        return this.cim + " (" + this.hossz + " s)"
+    }
+    
+}
+
+class Konyv implements Multimedia
+{
+    #cim: string;
+
+    constructor(cim: string)
+    {
+        this.#cim = cim;
+    }
+
+    get cim()
+    {
+        return this.#cim;
+    }
+
+    set cim(cim: string)
+    {
+        this.#cim = cim;
+    }
+
+    kiir(): void {
+        console.log(this.toString());
+    }
+    toString(): string {
+        return this.cim;
+    }
+    
+}
+
+let zene = new Zene('Let it go', 183);
+zene.kiir();
